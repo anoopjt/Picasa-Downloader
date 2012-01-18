0550c5ba362b6ab6c8d986d9ab7c5a01701c78a4
Puneeth Chaganti <punchagan@gmail.com>
2012-01-17 18:36:53 -0600

    ENH: Add a progress bar.

--- picasa_downloader.py
+++ picasa_downloader.py
@@ -8,6 +8,27 @@ from os.path import basename, join
 from os import mkdir
 from urlparse import urlsplit
 from BeautifulSoup import BeautifulStoneSoup
+import sys
+import time
+
+# Based on http://stackoverflow.com/a/3160819
+class ProgressBar(object):
+    def __init__(self, items):
+        self.display_width = 40
+        self.items = items
+
+    def update(self, n):
+        complete_frac = (float(n+1)/self.items)
+        complete = int(complete_frac * self.display_width)
+        remaining = self.display_width-complete
+        info = '%4.0f%% Done' %(complete_frac * 100,)
+        sys.stdout.write('[%s%s]%s' %('-' * complete, ' ' * remaining, info))
+        sys.stdout.flush()
+        if complete < self.display_width:
+            # add 2 to account for brackets [ ]
+            sys.stdout.write('\b' * (self.display_width+len(info)+2))
+        else:
+            sys.stdout.write('\n')
 
 feedurl = False
 url = raw_input('Enter the Picasa web album/feed url : ')
@@ -31,11 +52,13 @@ x = urlopen(url)
 y = x.read()
 tags = BeautifulStoneSoup(y).findAll('media:content')
 print len(tags), "pictures found!"
-for each in tags:
+progress_bar = ProgressBar(len(tags))
+for i, each in enumerate(tags):
     if each.has_key('url'):
         img = urlopen(each['url']).read()
         fname = join(name_album,basename(urlsplit(each['url'])[2]))
         output = open(fname,'wb')
         output.write(img)
         output.close()
+        progress_bar.update(i)
 
